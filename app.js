cdcaec4a864936f5160429fafeae49fc50c9e9fd
PabloGCW <cadenapg@gmail.com>
2022-05-03 15:50:42 -0300

    rutas entre views, links en botones y hover en Comprar

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ app.use(express.static(path.resolve("./public")));
 app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
 
 app.get("/", (req, res) =>{
-    res.sendFile(path.resolve("./views/home.html"));
+    res.sendFile(path.resolve("./views/index.html"));
 })
 
+app.post("/", (req, res) => {
+    res.sendFile(path.resolve("./views/index.html"));
+})
+
+app.get("/Registrarse", (req, res) => {
+    res.sendFile(path.resolve("./views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve("./views/login.html"));
+})
+
+app.get("/Carrito", (req, res) => {
+    res.sendFile(path.resolve("./views/productCart.html"));
+})
+
+app.get("/DetalleProducto", (req, res) => {
+    res.sendFile(path.resolve("./views/productDetail.html"))
+})
